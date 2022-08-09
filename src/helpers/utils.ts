be8d1fd5188ac5b839fcfb9ffa67910dd777fdf3
Rafael Vieira <rafaellvs322@gmail.com>
2022-08-09 01:38:24 -0300

    ENH: update utils file

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,24 +1,2 @@
-import NOTES from 'src/helpers/constants/notes'
-
-import Note from 'src/types/Note'
-import Direction from 'src/types/Direction'
-
-export const generateRandomNote = (): Note =>
-  NOTES[Math.floor(Math.random() * NOTES.length)]
-
-export const generateRandomDirection = (): Direction =>
-  Math.floor(Math.random() * 2) === 0 ? 'up' : 'down'
-
-export const getDistanceAnswer = (firstNote: Note, secondNote: Note, direction: Direction): number => {
-  if (firstNote === secondNote) return NOTES.length
-
-  const firstNoteIndex = NOTES.indexOf(firstNote)
-  const secondNoteIndex = NOTES.indexOf(secondNote)
-
-  return direction === 'up'
-    ? (secondNoteIndex - firstNoteIndex + NOTES.length) % NOTES.length
-    : (firstNoteIndex - secondNoteIndex + NOTES.length) % NOTES.length
-}
-
 export const padNumberWithZeros = (num: number, length: number) =>
   num.toString().padStart(length, '0')
