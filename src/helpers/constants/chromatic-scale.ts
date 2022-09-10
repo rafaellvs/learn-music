9f2ff39f55dfc28cbd678abd2a053da828997744
Rafael Vieira <rafaellvs322@gmail.com>
2022-09-09 21:06:19 -0300

    FEA: add CHROMATIC_SCALE constant

---
+++ src/helpers/constants/chromatic-scale.ts
@@ -0,0 +1,78 @@
+import { ChromaticScale } from 'src/types/ChromaticScale'
+
+const CHROMATIC_SCALE: ChromaticScale = [
+  {
+    value: 1,
+    natural: 'C',
+    sharp: 'B#',
+    flat: null,
+  },
+  {
+    value: 2,
+    natural: null,
+    sharp: 'C#',
+    flat: 'Db',
+  },
+  {
+    value: 3,
+    natural: 'D',
+    sharp: null,
+    flat: null,
+  },
+  {
+    value: 4,
+    natural: null,
+    sharp: 'D#',
+    flat: 'Eb',
+  },
+  {
+    value: 5,
+    natural: 'E',
+    sharp: null,
+    flat: 'Fb',
+  },
+  {
+    value: 6,
+    natural: 'F',
+    sharp: 'E#',
+    flat: null,
+  },
+  {
+    value: 7,
+    natural: null,
+    sharp: 'F#',
+    flat: 'Gb',
+  },
+  {
+    value: 8,
+    natural: 'G',
+    sharp: null,
+    flat: null,
+  },
+  {
+    value: 9,
+    natural: null,
+    sharp: 'G#',
+    flat: 'Ab',
+  },
+  {
+    value: 10,
+    natural: 'A',
+    sharp: null,
+    flat: null,
+  },
+  {
+    value: 11,
+    natural: null,
+    sharp: 'A#',
+    flat: 'Bb',
+  },
+  {
+    value: 12,
+    natural: 'B',
+    sharp: null,
+    flat: 'Cb',
+  },
+]
+
+export default CHROMATIC_SCALE
