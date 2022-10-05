a707d7b8e3db45054e838add3d8a4c952d3890ce
Rafael Vieira <rafaellvs322@gmail.com>
2022-10-05 12:14:09 -0300

    FEA: add new Questions type file

---
+++ src/types/Questions.ts
@@ -0,0 +1,22 @@
+import { Note, Direction } from 'src/types/ChromaticScale'
+
+type SemitoneIntervalQuestion = {
+  id: string,
+  firstNote: Note,
+  secondNote: Note,
+  direction: Direction,
+  answer: number,
+  userAnswer: null | number,
+  result: boolean,
+}
+
+type DiatonicScaleQuestion = {
+  majorScale: Array<Note>
+  userAnswer: Array<Note>
+  answerFeedback: {
+    isCorrect: boolean
+    wrongNotes: Array<Note>
+  }
+}
+
+export type { SemitoneIntervalQuestion, DiatonicScaleQuestion }
