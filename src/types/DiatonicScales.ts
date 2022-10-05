d9fb9347947b14b919494b4cc8798e641d33b25a
Rafael Vieira <rafaellvs322@gmail.com>
2022-10-04 22:14:36 -0300

    FEA: add DiatonicScaleAnswer type

--- src/types/DiatonicScales.ts
+++ src/types/DiatonicScales.ts
@@ -1,9 +1,14 @@
 import { Note } from 'src/types/ChromaticScale'
 
+type DiatonicScaleAnswer = {
+  isCorrect: boolean
+  wrongNotes: Array<Note>
+}
+
 type DiatonicScale = {
   name: string
   pattern: [number, number, number, number, number, number, number]
   roots: Array<Note>
 }
 
-export type { DiatonicScale }
+export type { DiatonicScale, DiatonicScaleAnswer }
